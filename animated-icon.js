50b05009a5db054eb51b6db53b92e6a339f11edd
aftxrlifx <96382604+its-Jaxx@users.noreply.github.com>
2023-10-28 11:51:12 -0700

    Updated to make it easier to understand.

--- animated-icon.js
+++ animated-icon.js
@@ -1,12 +1,10 @@
 let counter = 0; let frames = [
-  // You may change these links to your own links
-  // They will serve as frames for your animated icon
-  // The first link in the JavaScript file should always be the link as the HTML5 icon link
-  'https://cdn.discordapp.com/attachments/1085710779732467742/1085710815446978652/ramen_scene_1.png',
-  'https://cdn.discordapp.com/attachments/1085710779732467742/1085710822778617926/ramen_scene_2.png',
-  'https://cdn.discordapp.com/attachments/1085710779732467742/1085710844626747492/ramen_scene_3.png',
-  'https://cdn.discordapp.com/attachments/1085710779732467742/1085710859432624168/ramen_scene_4.png',
-  'https://cdn.discordapp.com/attachments/1085710779732467742/1085710879749840956/ramen_scene_5.png'
+  // Each image will be one (1) frame, going top to bottom
+  'image1.png',
+  'image2.png',
+  'image3.png',
+  'image4.png',
+  'image5.png'
 ]
 window.onload = function() {
   setInterval(function() {
@@ -15,5 +13,5 @@ window.onload = function() {
       counter++
     else
       counter = 1;
-  }, 5);
+  }, 5); // The number is the amount of ms each frame is displayed for.
 };
